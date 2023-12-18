f5b0589e8c979cebb0354c21221aaf0d445133aa
jkintner25 <jkintner25@gmail.com>
2023-12-18 17:34:50 -0600

    one more apostrophe

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
         <div className="flex items-center">
           <p className="text-5xl">404</p>
           <div className="w-[1px] h-[70px] border mx-6"></div>
-          <p className="text-justify">Sorry! We didn&apos;t find the event you're looking for. Double-check the URL and try again.</p>
+          <p className="text-justify">Sorry! We didn&apos;t find the event you&apos;re looking for. Double-check the URL and try again.</p>
         </div>
         <div className="mt-16">
           <GoHome />
