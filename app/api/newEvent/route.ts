fdd20d487b031c5ff72c3b2747f1818c934e2869
jkintner25 <jkintner25@gmail.com>
2023-12-19 02:21:48 -0600

    added another console log

--- app/api/newEvent/route.ts
+++ app/api/newEvent/route.ts
@@ -2,13 +2,15 @@ import { client } from "@/app/mongoClient";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
+  console.log('HIT api/newEvent')
   const body = await req.json()
   const { title, theme, datetime } = body;
+  console.log(title, theme, datetime);
 
   try {
     await client.connect();
-    const db = client.db("PotluckrDB");
-    const collection = db.collection("Events");
+    const db = await client.db("PotluckrDB");
+    const collection = await db.collection("Events");
     console.log(db, collection)
     const document = { title: title, theme: theme, datetime: datetime };
     const result = await collection.insertOne(document);
